1ce558cd6717f3a63ea61210fa6c389b2bde3d3c
yassinekarami <yassine.karami@outlook.com>
2018-12-12 08:07:01 +0000

    messagerie

---
+++ Messagerie/SmsSend/app/src/main/java/com/example/yassinekarami/smssend/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.yassinekarami.smssend;
+
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
+
+import android.telephony.SmsManager;
+
+import android.app.PendingIntent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editText;
+    private TextView textView;
+    private String message;
+    private SmsManager smsManager;
+    private Button sendBtn;
+
+    private final static String numero = "0647674120";
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.sms_message);
+        textView = (TextView) findViewById(R.id.text_view);
+        sendBtn = (Button) findViewById(R.id.btnSend);
+
+        textView.setText(numero);
+
+        if (checkPermission(Manifest.permission.SEND_SMS))
+        {
+            sendBtn.setEnabled(true);
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
+    }
+
+    public void SendMessageClick(View view){
+
+        // on récupère le message a envoyé
+        message = editText.getText().toString();
+        smsManager = SmsManager.getDefault();
+        String scAddress = null;
+        PendingIntent sentIntent = null, deliveryIntent = null;
+        smsManager.sendTextMessage(numero, scAddress, message, sentIntent, deliveryIntent);
+    }
+
+    private boolean checkPermission(String permission)
+    {
+        int ok = ContextCompat.checkSelfPermission(this,permission);
+        if (ok ==  PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        return false;
+    }
+
+
+
+
+
+}
